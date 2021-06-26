47aafea76f323ddba7d18581fa0f5be564ca3ea5
Ileen Fan <ifan2011@gmail.com>
2021-06-25 19:23:44 -0700

    added conditionals

--- hackervoice/index.js
+++ hackervoice/index.js
@@ -2,8 +2,16 @@ module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
     var password = req.query.password;
+
+    var response;
+
+    if (password == "letmein"){
+        response = "Access granted.";
+    } else {
+        response = "Access denied.";
+    }
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: password
+        body: response
     };
 }
