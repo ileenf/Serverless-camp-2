7c575ecdb090e98eb79c74866c9cff4913ce4398
Ileen Fan <ifan2011@gmail.com>
2021-07-01 21:19:48 -0700

    add face api

--- emotionalgifs/index.js
+++ emotionalgifs/index.js
@@ -1,5 +1,5 @@
 var multipart = require('parse-multipart');
-
+var fetch = require('node-fetch');
 
 
 module.exports = async function (context, req) {
@@ -16,10 +16,41 @@ module.exports = async function (context, req) {
 
     var imageData = parts[0].data;
 
-    var convertedResult = Buffer.from(imageData).toString('base64');
-
+    var result = await analyzeImage(imageData);
     context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: convertedResult
+        body: {
+            result
+        }
     };
+    console.log(result)
+    context.done(); 
+
+    
+}
+
+async function analyzeImage(img){
+    const subscriptionKey = process.env.SUBSCRIPTIONKEY;
+    const uriBase = process.env.ENDPOINT + '/face/v1.0/detect';
+
+
+
+    let params = new URLSearchParams({
+        'returnFaceId': 'true',
+        'returnFaceAttributes': 'emotion'     //FILL IN THIS LINE
+    })
+
+    //COMPLETE THE CODE
+    let resp = await fetch(uriBase + '?' + params.toString(), {
+        method: 'POST',  //WHAT TYPE OF REQUEST?
+        body: img,  //WHAT ARE WE SENDING TO THE API?
+        
+            //ADD YOUR TWO HEADERS HERE
+        headers: {
+            'Content-Type': 'application/octet-stream',
+            'Ocp-Apim-Subscription-Key': subscriptionKey
+        }
+    })
+
+    let emotionData = await resp.json();
+    return emotionData;
 }
