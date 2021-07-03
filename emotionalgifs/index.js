6faa26697e8a205bbf77e67c1c360e9b929a611a
Ileen Fan <ifan2011@gmail.com>
2021-07-02 18:16:12 -0700

    main emotion change

--- emotionalgifs/index.js
+++ emotionalgifs/index.js
@@ -18,16 +18,16 @@ module.exports = async function (context, req) {
 
     var result = await analyzeImage(imageData);
 
-    let emotions = result[0].faceAttributes.emotion;
+    let emotions = result.faceAttributes.emotion;
 
     let objects = Object.values(emotions);
 
     const main_emotion = Object.keys(emotions).find(key => emotions[key] === Math.max(...objects));
 
     context.res = {
-        body: {
+        body: 
             main_emotion
-        }
+        
     };
     console.log(result)
     context.done(); 
@@ -36,11 +36,11 @@ module.exports = async function (context, req) {
 }
 
 async function analyzeImage(img){
-    const subscriptionKey = process.env.SUBSCRIPTIONKEY;
-    const uriBase = process.env.ENDPOINT + '/face/v1.0/detect';
+    // const subscriptionKey = process.env.SUBSCRIPTIONKEY;
+    // const uriBase = process.env.ENDPOINT + '/face/v1.0/detect';
 
-    // const subscriptionKey = 'c3168e61e37749f591e59f318f475148'
-    // const uriBase = 'https://faceapiserverless.cognitiveservices.azure.com/face/v1.0/detect';
+    const subscriptionKey = 'c3168e61e37749f591e59f318f475148'
+    const uriBase = 'https://faceapiserverless.cognitiveservices.azure.com/face/v1.0/detect';
 
 
 
